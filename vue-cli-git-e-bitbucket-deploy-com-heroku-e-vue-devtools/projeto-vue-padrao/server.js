e0a9c9ea1cdc45cc1d33134e92f9dd68df68c1bf
murilonicemento <murilo_nascimento22@outlook.com>
2024-04-06 18:02:47 -0300

    criação servidor web

---
+++ vue-cli-git-e-bitbucket-deploy-com-heroku-e-vue-devtools/projeto-vue-padrao/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// responder a requisições feitas para o servidor web por meio da rota get
+
+app.get("/", function (req, resp) {
+    resp.send("Olá, eu sou um servidor web em Node com Express");
+});
+
+app.listen(3000, function () {
+    console.log("Servidor web rodando com Express");
+});
